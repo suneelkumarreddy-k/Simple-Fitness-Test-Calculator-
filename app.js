e730d49c55378761992eae1a136bc61bbc224c1f
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2021-12-29 13:41:15 +0530

    Update app.js
    Code changed

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 function sft() {
 var start = performance.now();
-document.getElementById("reps").innerHTML = Math.ceil(document.getElementById("age").value / 2);
+let karma = 3;
+let age = document.getElementById("age").value;
+let avatar = age/karma;
+document.getElementById("reps").innerHTML = Math.ceil(avatar);
 var end = performance.now();
 var wpn = end-start;
 alert("Calculated in "+wpn+" ms 😎");
